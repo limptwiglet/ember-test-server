5887e645f2dd1a53e18feaf0f37ee36024f6ae04
Mark Gerrard <limptwiglet@gmail.com>
2014-02-11 11:47:18 +0000

    Added reset and start helpers

--- ember-test-server.js
+++ ember-test-server.js
@@ -1,24 +1,33 @@
 (function () {
-	var server;
+    var server;
 
-	Ember.Test.registerAsyncHelper('serverResponse', function (app, verb, url, body, status) {
-		if (!server) createServer();
+    Ember.Test.registerHelper('serverResponse', function (app, verb, url, body, status) {
+        if (!server) createServer();
 
-		status = status || 200;
-		body = (typeof body === 'string') ? body : JSON.stringify(body);
+        status = status || 200;
+        body = (typeof body === 'string') ? body : JSON.stringify(body);
 
-		server.respondWith(verb, url, [
-				status,
-				{'Content-Type': 'application/json'},
-				body
-			]);
+        server.respondWith(verb.toUpperCase(), url, [
+                status,
+                {'Content-Type': 'application/json'},
+                body
+            ]);
 
-		server.respond();
+        server.respond();
+    });
 
-		return wait(app);
-	});
 
-	function createServer () {
-		server = sinon.fakeServer.create();
-	}
+    Ember.Test.registerHelper('serverReset', function () {
+        server.restore();
+    });
+
+    Ember.Test.registerHelper('serverStart', function () {
+        createServer();
+    });
+
+    function createServer () {
+        server = sinon.fakeServer.create();
+        server.autoRespond = true;
+        server.autoRespondAfter = 1;
+    }
 })();
