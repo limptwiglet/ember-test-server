9958c594ff361279bd4cae5a105428cc0ebff730
Mark Gerrard <mark@believe.in>
2014-03-03 15:44:44 +0000

    Allow verb or body to be a function

--- ember-test-server.js
+++ ember-test-server.js
@@ -2,16 +2,27 @@
     var server;
 
     window.serverResponse = function (verb, url, body, status) {
+    	var fnResponse = false;
         if (!server) createServer();
 
         status = status || 200;
-        body = (typeof body === 'string') ? body : JSON.stringify(body);
 
-        server.respondWith(verb.toUpperCase(), url, [
-                status,
-                {'Content-Type': 'application/json'},
-                body
-            ]);
+        if (typeof verb !== 'function' && typeof body !== 'function') {
+			body = (typeof body === 'string') ? body : JSON.stringify(body);
+			verb = verb.toUpperCase();
+
+			server.respondWith(verb.toUpperCase(), url, [
+					status,
+					{'Content-Type': 'application/json'},
+					body
+				]);
+        } else if (typeof verb === 'function') {
+       		server.respondWith(method);
+        } else if (typeof body === 'function') {
+       		server.respondWith(verb.toUpperCase(), url, body);
+
+        }
+
 
         server.respond();
     };
